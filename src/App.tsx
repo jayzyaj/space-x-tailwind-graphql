8087ba4925db2f74f680199b135c16b43cb4cf64
John Clifford de Vera <johnclifforddevera@johns-mbp.mynet>
2021-08-29 20:47:06 +0800

    improve css

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,7 @@ import { SpaceLaunchList } from './components/SpaceLaunchList';
 function App() {
   return (
     <div className="App m-5">
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8">
-        <SpaceLaunchList />
-      </div>
+      <SpaceLaunchList />
     </div>
   );
 }
--- src/components/SpaceLaunchList.tsx
+++ src/components/SpaceLaunchList.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { EXCHANGE_RATES } from '../services/query';
+import './styles/space-launch-list.css';
 
 function SpaceLaunchList() {
   const { loading, error, data } = useQuery(EXCHANGE_RATES, {
     variables: { limit: 10 },
   });
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return (
+    <div className="flex justify-center items-center flex-col">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-black"></div>
+      <p className="text-black text-lg font-black mt-3">Loading Space-X Launches</p>
+    </div>
+  );
   if (error) return <p>Error :(</p>;
 
-  return data.launchesPast.map(({ id, mission_name, rocket, links }: any) => (
-    <div className="hover:border-blue-200 hover:bg-blue-50 hover:bg-opacity-50 hover:cur bg-white rounded-xl p-8 border-gray-100 border-opacity-90 border-2" key={id}>
-      <p className="text-black text-lg font-black">
-        {mission_name}
-      </p>
-      <p className="text-base font-extralight tracking-widest">
-        {rocket.rocket_name}
-      </p>
-      {!!links.flickr_images.length && <img alt="Ship" className="bg-gray-300 flex flex-shrink-0" src={links.flickr_images[0]} />}
+  return (
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8">
+      {data.launchesPast.map(({ id, mission_name, rocket, links }: any) => (
+        <div className="space-card hover-ease-scale" key={id}>
+          <p className="text-black text-lg font-black">
+            {mission_name}
+          </p>
+          <p className="text-base font-extralight tracking-widest">
+            {rocket.rocket_name}
+          </p>
+          {!!links.flickr_images.length && <img alt="Ship" className="bg-gray-300 flex flex-shrink-0" src={links.flickr_images[0]} />}
+        </div>
+      ))}
     </div>
-  ));
+  );
 }
 
 export { SpaceLaunchList };
